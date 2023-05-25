f220bc41b485148b6bdac6e5bd11e6b23cbda1af
Joseph Flynn <joseph.f.flynn@gmail.com>
2023-05-25 12:46:52 +0100

    models rspec

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# require 'faker'
+
+# puts "Cleaning database..."
+# Movie.destroy_all
+# List.destroy_all
+# Bookmark.destroy_all
+
+# puts "Creating restaurants..."
+
+# 20.times do |movie|
+#   movie = Movie.create(title: Faker::Movie.title , overview: Faker::Movie.quote, rating: rand(1..10))
+#   puts "Created #{movie.name}"
+# end
+# puts "Finished creating movies!"
+
+# 5.times do |list|
+#   list = List.create(name: Faker::Music.genre)
+# end
+
+# puts "Finished creating lists(genres)..."
+
+# 40.times do |review|
+#   review = Review.create(rating: rand(1..5), content: Faker::Restaurant.review, restaurant: Restaurant.all.sample)
+#   puts "Created #{review.rating}"
+# end
+# puts "All Done!"
